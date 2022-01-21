4fe9c45ae8ceae96432124abb0f91d10cd054e5c
Danil Maiorov <damayorov93@gmail.com>
2022-01-22 00:33:56 +0500

    correct main Slider

--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,55 +1,45 @@
+import { animate } from './helpers'
+
 export const slider = () => {
 
     const sliderContainer = document.querySelector('.top-slider')
     let slides = document.querySelectorAll('.item')
-    let width = window.getComputedStyle(sliderContainer).width
+    const width = sliderContainer.style.width
+
     const interval = 3000
-    let slideId
-    let index = 1
+    let clearInterval
+    let currentSlide = 0
 
-    sliderContainer.style.display = 'inline-flex'
-    slides.forEach(slide => {
-        slide.style.width = width
-        slide.style.height = '600px'
+    window.addEventListener('resize', () => {
+        slides.forEach(slide => {
+            slide.style.width = width
+        })
     })
-    const firstClone = slides[0].cloneNode(true) //создадим клонов первого и последнего слайдов со всеми свойствами
-    const lastClone = slides[slides.length - 1].cloneNode(true)
-
-    firstClone.id = 'first-clone' //дадим id клонам
-    lastClone.id = 'last-clone'
-    sliderContainer.append(firstClone)
-    sliderContainer.prepend(lastClone)
-
-    const slideWidth = slides[index].clientWidth
-    sliderContainer.style.transform = `translateX(${-slideWidth * index}px)`
-    const startSlide = () => {
-        slideId = setInterval(() => {
-            index++
-            sliderContainer.style.transform = `translateX(${-slideWidth * index}px)`
-            sliderContainer.style.transition = '0.7s'
-        }, interval)
+    const prevSlide = (elems, index, strClass) => {
+        elems[index].style.display = strClass
+    };
+    const nextSlide = (elems, index, strClass) => { 
+        elems[index].style.display = strClass
+        animate({
+            duration: 600,
+            timing(timeFraction) {
+            return timeFraction
+            },
+            draw(progress) {
+                elems[index].style.opacity = 0 + progress
+            } 
+        })
+    };
+    const autoSlide = () => {
+        prevSlide(slides, currentSlide, 'none');
+        currentSlide++
+        if(currentSlide >= slides.length) {
+            currentSlide = 0
         }
-        sliderContainer.addEventListener('transitionend', () => {
-            slides = document.querySelectorAll('.item')
-            if(slides[index].id === firstClone.id) {
-                sliderContainer.style.transition = 'none'
-                index = 1
-                sliderContainer.style.transform = `translateX(${-slideWidth * index}px)`
-            }
-        })
-
-        startSlide()
-        window.addEventListener('resize', startSlide)
-        sliderContainer.addEventListener('mouseenter', () => {
-            clearInterval(slideId)
-        })
-        sliderContainer.addEventListener('mouseleave',startSlide)
-
-        
-        
-    const push = document.querySelector('.push100')
-    push.style.height = '100px'
-    push.style.marginTop = '-5px'
-
-
+        nextSlide(slides, currentSlide, 'block');
+    };
+    const startSlide = () => {
+        clearInterval = setInterval(autoSlide, interval)
+    }
+    startSlide()
 }

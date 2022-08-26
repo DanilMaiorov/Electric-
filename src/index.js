025d479599975bf4c381bc87c1423bd3c8d145a1
Danil Maiorov <damayorov93@gmail.com>
2022-08-26 22:49:12 +0300

    correct

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import { modal } from './modules/modal';
 import { menu } from './modules/menuScroll';
 import { accordeon } from './modules/accordeon';
 import { sendForm } from './modules/sendForm';
-import { swiper } from './modules/swiper';
+//import { swiper } from './modules/swiper';
 import { slider } from './modules/slider';
 import { carousel } from './modules/carousel';
 
--- src/modules/slider.js
+++ src/modules/slider.js
@@ -1,24 +1,24 @@
 export const slider = () => {
 
-    const sliderContainer = document.querySelector('.top-slider')
-    let slides = document.querySelectorAll('.item')
-    let width = window.getComputedStyle(sliderContainer).width
-    const interval = 3000
-    let slideId
-    let index = 1
+    const sliderContainer = document.querySelector('.top-slider');
+    let slides = document.querySelectorAll('.item');
+    let width = window.getComputedStyle(sliderContainer).width;
+    const interval = 3000;
+    let slideId;
+    let index = 1;
 
-    sliderContainer.style.display = 'inline-flex'
+    sliderContainer.style.display = 'inline-flex';
     slides.forEach(slide => {
-        slide.style.width = width
-        slide.style.height = '600px'
+        slide.style.width = width;
+        slide.style.height = '600px';
     })
-    const firstClone = slides[0].cloneNode(true) //создадим клонов первого и последнего слайдов со всеми свойствами
-    const lastClone = slides[slides.length - 1].cloneNode(true)
+    const firstClone = slides[0].cloneNode(true); //создадим клонов первого и последнего слайдов со всеми свойствами
+    const lastClone = slides[slides.length - 1].cloneNode(true);
 
-    firstClone.id = 'first-clone' //дадим id клонам
-    lastClone.id = 'last-clone'
-    sliderContainer.append(firstClone)
-    sliderContainer.prepend(lastClone)
+    firstClone.id = 'first-clone'; //дадим id клонам
+    lastClone.id = 'last-clone';
+    sliderContainer.append(firstClone);
+    sliderContainer.prepend(lastClone);
 
     const slideWidth = slides[index].clientWidth
     sliderContainer.style.transform = `translateX(${-slideWidth * index}px)`
@@ -39,13 +39,13 @@ export const slider = () => {
         })
 
         sliderContainer.addEventListener('mouseenter', () => {
-            clearInterval(slideId)
-        })
-        sliderContainer.addEventListener('mouseleave',startSlide)
+            clearInterval(slideId);
+        });
+        sliderContainer.addEventListener('mouseleave',startSlide);
 
-        startSlide()
+        startSlide();
         
-    const push = document.querySelector('.push100')
-    push.style.height = '100px'
-    push.style.marginTop = '-5px'
+    const push = document.querySelector('.push100');
+    push.style.height = '100px';
+    push.style.marginTop = '-5px';
 }
